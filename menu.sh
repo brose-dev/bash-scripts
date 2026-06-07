7cb23d28519caa7339e2003177fc7c37dffa96f1
brose-dev <rosebritania102@gmail.com>
2026-06-07 19:13:04 -0400

    Updated menu script to take inputs for script files that require inputs

--- menu.sh
+++ menu.sh
@@ -28,18 +28,61 @@ while true; do
 	read choice
 
 	case $choice in
-		1) bash "$SCRIPT_DIR/file_organizer.sh" ;;
-		2) bash "$SCRIPT_DIR/log_parser.sh" ;;
+		1)
+			echo -n "Enter source folder path: "
+			read src
+			src="${src/#\~/$HOME}"
+			bash "$SCRIPT_DIR/file_organizer.sh" "$src"
+			;;
+		2)
+			echo -n "Enter log file path: "
+			read logfile
+			logfile="${logfile/#\~/$HOME}"
+			bash "$SCRIPT_DIR/log_parser.sh" "$logfile"
+			;;
 		3) bash "$SCRIPT_DIR/disk_checker.sh" ;;
-		4) bash "$SCRIPT_DIR/server_health.sh" ;;
-		5) bash "$SCRIPT_DIR/backup.sh" ;;
-		6) bash "$SCRIPT_DIR/user_checker.sh" ;;
+		4)
+			echo -n "Enter process name to check: "
+			read proc
+			bash "$SCRIPT_DIR/server_health.sh" "$proc"
+			;;
+		5)
+			echo -n "Enter source folder to backup: "
+			read sou
+			echo -n "Enter backup destination: "
+			read dest
+			sou="${sou/#\~/$HOME}"
+			dest="${dest/#\~/$HOME}"
+			bash "$SCRIPT_DIR/backup.sh" "$sou" "$dest"
+			;;
+		6)
+			echo -n "Enter username to check: "
+			read uname
+			bash "$SCRIPT_DIR/user_checker.sh" "$uname"
+			;;
 		7) bash "$SCRIPT_DIR/service_monitor.sh" ;;
 		8) bash "$SCRIPT_DIR/system_info.sh" ;;
-		9) bash "$SCRIPT_DIR/bulk_server_check.sh" ;;
-		10) bash "$SCRIPT_DIR/log_rotation.sh" ;;
+		9)
+			echo -n "Enter path to hosts file: "
+			read hostsfile
+			hostsfile="${hostsfile/#\~/$HOME}"
+			bash "$SCRIPT_DIR/bulk_server_check.sh" "$hostsfile"
+			;;
+		10)
+			echo -n "Enter log directory path: "
+			read logdir
+			echo -n "Enter archive destination path: "
+			read archivedir
+			logdir="${logdir/#\~/$HOME}"
+			archivedir="${archivedir/#\~/$HOME}"
+			bash "$SCRIPT_DIR/log_rotation.sh" "$logdir" "$archivedir"
+			;;
 		11) bash "$SCRIPT_DIR/failed_login_monitor.sh" ;;
-		12) bash "$SCRIPT_DIR/server_report_emailer.sh" ;;
+		12)
+			echo -n "Enter recipient email address: "
+			read email
+			bash "$SCRIPT_DIR/server_report_emailer.sh" "$email"
+			;;
 		0) echo "Goodbye!"; exit 0 ;;
 		*) echo "Invalid option. Try again." ;;
 	esac
